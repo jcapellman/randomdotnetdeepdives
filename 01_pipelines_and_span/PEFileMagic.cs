df21ad4df2c99817c0099bf5b7a14bb2c02470ef
Jarred Capellman <jcapellman@hotmail.com>
2018-07-11 21:03:23 -0500

    Added: Initial work on PE File Magic Check using Pipelines and Span

---
+++ 01_pipelines_and_span/PEFileMagic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipelines;
+
+using System.Threading.Tasks;
+
+namespace pipelines_span
+{
+    public static class PEFileMagic
+    {
+        private static readonly byte[] MZ_HEADER = {77, 90};    // MZ
+
+        public static async Task<bool> IsPEAsync(string fileName)
+        {
+            using (var fileStream = new FileStream(fileName, FileMode.Open))
+            {
+                var pipe = new Pipe();
+
+                var memory = pipe.Writer.GetMemory(2);
+
+                await fileStream.ReadAsync(memory);
+
+                return memory.Slice(0, 2).Span.SequenceEqual(MZ_HEADER);
+            }
+        }
+    }
+}
---
+++ 01_pipelines_and_span/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+using System.Threading.Tasks;
+
+namespace pipelines_span
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine($"{args[0]} does not exist");
+
+                return;
+            }
+
+            var result = await PEFileMagic.IsPEAsync(args[0]);
+
+            Console.WriteLine($"{args[0]} is PE: {result}");
+        }
+    }
+}
