76abbb1fcda490709abada034086ae4eec45bcae
Jarred Capellman <jcapellman@hotmail.com>
2018-07-12 22:44:13 -0500

    Added: Additional testing scenarios

--- 01_pipelines_and_span/PEFileMagic.cs
+++ 01_pipelines_and_span/PEFileMagic.cs
@@ -1,35 +1,51 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using BenchmarkDotNet.Attributes.Jobs;
 
 namespace pipelines_span
 {
-    public static class PEFileMagic
+    [CoreJob]
+    public class PEFileMagic
     {
         private static readonly byte[] MZ_HEADER = {77, 90};    // MZ
+        
+        [Params("test1.exe", "test2.exe", "test3.exe")]
+        public string fileName;
+        
+        [Benchmark]
+        public bool IsPESpan()
+        {
+            using (var fileStream = new FileStream(Path.Combine(@"..\..\..\..\..\..\..\Samples", fileName), FileMode.Open))
+            {
+                Span<byte> memory = new byte[2];
+                    
+                fileStream.Read(memory);
+
+                return memory.Slice(0, 2).SequenceEqual(MZ_HEADER);
+            }
+        }
 
         [Benchmark]
-        public static async Task<bool> IsPEAsync(string fileName)
+        public bool IsPEMemorySpan()
         {
-            using (var fileStream = new FileStream(fileName, FileMode.Open))
+            using (var fileStream = new FileStream(Path.Combine(@"..\..\..\..\..\..\..\Samples", fileName), FileMode.Open))
             {
-                var arr = new byte[2];
-
-                Memory<byte> memory = arr;
-                    
-                await fileStream.ReadAsync(memory);
+                Memory<byte> memory = new byte[2];
+                
+                fileStream.Read(memory.Span);
 
                 return memory.Slice(0, 2).Span.SequenceEqual(MZ_HEADER);
             }
         }
 
         [Benchmark]
-        public static bool IsPE(string fileName)
+        public bool IsPEBinaryReader()
         {
-            using (var fileStream = new FileStream(fileName, FileMode.Open))
+            using (var fileStream = new FileStream(Path.Combine(@"..\..\..\..\..\..\..\Samples", fileName), FileMode.Open))
             {
                 var reader = new BinaryReader(fileStream);
 
--- 01_pipelines_and_span/Program.cs
+++ 01_pipelines_and_span/Program.cs
@@ -1,46 +1,12 @@
-﻿using System;
-using System.IO;
-
-using System.Threading.Tasks;
+﻿using BenchmarkDotNet.Running;
 
 namespace pipelines_span
 {
     class Program
     {
-        private static async Task Main(string[] args)
+        private static void Main(string[] args)
         {
-            if (!File.Exists(args[0]))
-            {
-                Console.WriteLine($"{args[0]} does not exist");
-
-                return;
-            }
-
-            DateTime start = DateTime.Now;
-
-            for (var x = 0; x < 1000; x++) {
-                var result = await PEFileMagic.IsPEAsync(args[0]);
-            }
-            
-            start = DateTime.Now;
-
-            for (var x = 0; x < 1000; x++)
-            {
-                var result = PEFileMagic.IsPE(args[0]);
-            }
-
-            Console.WriteLine(DateTime.Now.Subtract(start).TotalMilliseconds);
-
-            start = DateTime.Now;
-
-            for (var x = 0; x < 1000; x++)
-            {
-                var result = await PEFileMagic.IsPEAsync(args[0]);
-            }
-
-            Console.WriteLine(DateTime.Now.Subtract(start).TotalMilliseconds);
-
-            Console.ReadKey();
+            var summary = BenchmarkRunner.Run<PEFileMagic>();
         }
     }
 }
