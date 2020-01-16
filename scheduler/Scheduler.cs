ffa727cb78b047a8a0a5f0f2205013c179ff7fd8
Jarred Capellman <jcapellman@hotmail.com>
2020-01-15 19:20:17 -0600

    Added: Scheduler class

---
+++ scheduler/Scheduler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+using XGBoost;
+
+namespace scheduler
+{
+    public class Scheduler
+    {
+        private Dictionary<Guid, Worker> _workers = new Dictionary<Guid, Worker>();
+
+        private ConcurrentQueue<int> _pendingQueues = new ConcurrentQueue<int>();
+
+        private ConcurrentDictionary<Guid, bool> _completedWork = new ConcurrentDictionary<Guid, bool>();
+
+        public Scheduler()
+        {
+            var xgbClassifier = new XGBClassifier();
+
+            foreach (var worker in _workers.Values)
+            {
+                worker.Initialize(xgbClassifier);
+
+                worker.NotBusy += Worker_NotBusy;
+            }
+        }
+
+        private void Worker_NotBusy(object sender, Guid e)
+        {
+            lock (_pendingQueues)
+            {
+                if (_pendingQueues.IsEmpty)
+                {
+                    return;
+                }
+
+                _pendingQueues.TryDequeue(out var res);
+
+                _workers[e].Run(res);
+            }
+        }
+
+        public bool Run(int x)
+        {
+            var id = Guid.NewGuid();
+
+            while (!_completedWork.ContainsKey(id))
+            {
+                Thread.Sleep(50);
+            }
+
+            _completedWork.TryRemove(id, out var val);
+
+            return val;
+        }
+    }
+}
---
+++ scheduler/Worker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using XGBoost;
+
+namespace scheduler
+{
+    public class Worker
+    {
+        private XGBClassifier _classifier;
+
+        public Guid ID { get; set; }
+
+        public void Initialize(XGBClassifier classifier)
+        {
+            _classifier = classifier;
+            ID = Guid.NewGuid();
+        }
+
+        public bool Run(int x)
+        {
+            return x % 2 == 0;
+        }
+
+        public event EventHandler<Guid> NotBusy;
+    }
+}
